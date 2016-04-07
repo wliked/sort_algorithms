747239ffb2b881007db893960dfea3221605ed52
wangliang13 <wangliang13@lenovo.com>
2016-04-07 23:19:24 +0800

    update source code created date

--- sort_algorithms.c
+++ sort_algorithms.c
@@ -2,7 +2,7 @@
 //*
 //* Copyright (c) 2016 : wang liang
 //* License : Distributed under the GNU General Public License
-//* created on : 4/02/2016,  by wang liang (wliked@outlook.com)
+//* created on : 4/07/2016,  by wang liang (wliked@outlook.com)
 //* 
 //*************************************************************
 
